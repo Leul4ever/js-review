360760bcfd8f056a13a6283c20e0a99e13268342
Liul-Abera <liulaber43@gmail.com>
2024-09-12 23:01:53 -0400

    promise

--- starter/script.js
+++ starter/script.js
@@ -195,6 +195,8 @@ function getTotalReviewCount(book){
 }
 console.log(getTotalReviewCount(book))
 */
+
+/*
 const books = getBooks();
 books
 const ar =[1,2,3,4,5];
@@ -257,6 +259,26 @@ booksAfterDelete
 //books After Delete
 const booksAfterUpdate=booksAfterDelete.map((book)=>book.id===1 ? {...book, pages:120}:book);
 booksAfterUpdate
+
+*/
+
+// fetch("https://jsonplaceholder.typicode.com/todos")
+// .then((res)=>res.json())
+// .then((data)=>console.log(data));
+// console.log("Leul")
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos")
+  const data =await res.json();
+  console.log(data)
+
+  return data;
+  
+}
+ const todos =getTodos();
+ console.log(todos)
+
+
 
 
 
